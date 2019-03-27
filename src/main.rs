7185f7c78597845ec270aff39170d68ea62f37d2
David Garfinkle <david@kn0x.io>
2019-03-27 19:19:00 -0400

    wip..

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+extern crate itertools;
+use itertools::Itertools;
+
+pub struct Score {
+    pub vectors: Vec<[Note; 2]>,
+}
+
+pub struct Note {
+    pub onset: f32,
+    pub offset: f32,
+    pub pitchB40: i32,
+}
+
+impl From<Vec<Note>> for Score {
+    fn from(notes: Vec<Note>) -> Self {
+        let mut vecs: Vec<[Note; 2]>;
+        for window in notes.windows(15) {
+            for v in window.iter().combinations(2) {
+                new_vec = [Note; 2];
+            }
+        }
+        Self {
+            vectors: vecs
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
+
+/*
+        for window in notes.windows(15) {
+            for (i, left_note) in window.iter().enumerate() {
+                for right_note in &window.iter().
+                    vecs.push([left_note, right_note]);
+                }
+            }
+        }
+        */
